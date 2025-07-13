0e527951fcb52f8f11fe4bb5a835492e8cef60bb
BadLuckZ <thanagorn8802@gmail.com>
2025-07-13 21:31:52 +0700

    fix: fix due to pre-push

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 // @ts-check
-import tailwindcss from "@tailwindcss/vite";
-
 import node from "@astrojs/node";
 import react from "@astrojs/react";
-
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+
   adapter: node({
     mode: "standalone",
   }),
