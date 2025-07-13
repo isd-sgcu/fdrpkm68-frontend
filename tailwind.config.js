38dabd652a7bd85febe92ddcb3a71c20c9d133af
Sanpaphat Porntongprasert <sanpaphat14@gmail.com>
2025-07-13 23:37:31 +0700

    feat/fest/OrbitronFix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      fontFamily: {
+
+      css: {
+        ':root': { // This target ensures the variables are globally available
+          '--font-orbitron': '"Orbitron Variable", sans-serif',
+          '--font-ibm-plex-sans-thai': '"IBM Plex Sans Thai", sans-serif',
+          '--font-zen-dots': '"Zen Dots", cursive',
+        },
       },
+
     },
   },
   plugins: [],
