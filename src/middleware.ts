3acdc8f78069d90cc2740f0b40730b35f4f1114e
Borworntat Dendumrongkul <mastericez890@gmail.com>
2025-07-14 21:17:12 +0700

    feat: middleware

---
+++ src/middleware.ts
@@ -0,0 +1,50 @@
+import { defineMiddleware } from "astro:middleware";
+import jwt from "jsonwebtoken";
+
+import { nonProtectRoutes } from "@/constants/NonProtectedRoutes";
+
+export const onRequest = defineMiddleware(async (context, next) => {
+  const url = context.url;
+
+  console.log(`middleware from ${context.url.pathname}`);
+
+  const isProtectedRoute = !nonProtectRoutes.some((route) => {
+    return url.pathname.startsWith(route);
+  });
+  if (!isProtectedRoute) {
+    return next();
+  }
+
+  const cookie = context.request.headers.get("cookie") ?? "";
+  const token = parseCookie(cookie).token;
+
+  if (token && isValidToken(token)) {
+    return next();
+  }
+
+  return new Response(null, {
+    status: 302,
+    headers: {
+      Location: "/login",
+    },
+  });
+});
+
+function parseCookie(cookie: string): Record<string, string> {
+  return Object.fromEntries(
+    cookie
+      .split("; ")
+      .map((c) => c.split("="))
+      .map(([k, v]) => [k, decodeURIComponent(v)])
+  );
+}
+
+function isValidToken(token: string): boolean {
+  try {
+    jwt.verify(token, import.meta.env.JWT_SECRET || "");
+    return true;
+  } catch (error) {
+    console.error("Invalid token:", error);
+    return false;
+  }
+}
