af9a9c44ffeda30115603a2db827e332f3122205
Borworntat Dendumrongkul <mastericez890@gmail.com>
2025-07-15 23:55:06 +0700

    feat: big brain middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,18 +5,24 @@ import { nonProtectRoutes } from "@/constants/NonProtectedRoutes";
 
 export const onRequest = defineMiddleware(async (context, next) => {
   const url = context.url;
-
-  console.log(`middleware from ${context.url.pathname}`);
-
-  const isProtectedRoute = !nonProtectRoutes.some((route) => {
+  const isNonProtectedRoute = nonProtectRoutes.some((route) => {
     return url.pathname.startsWith(route);
   });
-  if (!isProtectedRoute) {
-    return next();
-  }
 
   const cookie = context.request.headers.get("cookie") ?? "";
   const token = parseCookie(cookie).token;
+
+  if (isNonProtectedRoute && token && isValidToken(token)) {
+    return new Response(null, {
+      status: 302,
+      headers: {
+        Location: "/firstdate/home",
+      },
+    });
+  }
+  if (isNonProtectedRoute) {
+    return next();
+  }
 
   if (token && isValidToken(token)) {
     context.locals.user = jwt.decode(token) as App.Locals["user"];
