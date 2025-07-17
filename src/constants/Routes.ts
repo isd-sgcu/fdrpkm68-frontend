82d9fc3cd02bd2da127c959be7beb393f09915f9
Peerapat Patcharamontree <peerapat.pat@ext.skooldio.dev>
2025-07-18 01:45:14 +0700

    feat: add nonStartedRoutes time constraint whitelist
    (cherry picked from commit 77bf8bfd89115e4e4225b0afe7c472c0298048b7)

--- src/constants/Routes.ts
+++ src/constants/Routes.ts
@@ -7,4 +7,12 @@ export const nonProtectRoutes = [
   "/staff/login",
 ];
 
-export const nonStartedRoutes = ["/rpkm", "/fest"];
+export const nonStartedRoutes = ["/rpkm", "/fest"] as const;
+
+export type NonStartedRoute = (typeof nonStartedRoutes)[number];
+
+// NOTE: If you need to change this please also consider update time in backend
+export const nonStartedRoutesTimeWhitelist: Record<NonStartedRoute, Date> = {
+  "/fest": new Date("2025-07-18T19:00:00+07:00"),
+  "/rpkm": new Date("2025-07-20T19:00:00+07:00"),
+};
